176a22e5de9af1ba91e291b36fbe849bfe868346
TJ Spinks <tjspinks@gmail.com>
2026-06-07 18:45:34 -0500

    javascript error needed fixing

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,21 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@supabase/supabase-js'],
+    include: [
+      '@supabase/supabase-js',
+      '@supabase/realtime-js',
+      '@supabase/storage-js',
+      '@supabase/functions-js',
+      '@supabase/postgrest-js',
+    ],
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          supabase: ['@supabase/supabase-js'],
+        },
+      },
+    },
   },
 })
